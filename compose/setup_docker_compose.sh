5c220687516025c9018f7d16df536090af0fe5d3
amrkedra <amrkedra.1993@gmail.com>
2024-09-19 16:16:59 +0300

    updated the scripts

--- compose/setup_docker_compose.sh
+++ compose/setup_docker_compose.sh
@@ -9,18 +9,20 @@ echo "Installing pyhton dependecies for compose ...."
 
 sudo apt install -y python3 python3-pip python3-venv python3-setuptools -y
 
-# Check if Docker Compose is installed
-echo "Downloading Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Ensure the binary is executable
-sudo chmod 777 /usr/local/bin/docker-compose
 
 # Verify the installation
-if command -v docker-compose &> /dev/null; then
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose not found, installing..."
+
+    # Download the latest Docker Compose binary
+    sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    
+    # Make the binary executable
+    sudo chmod 777 /usr/local/bin/docker-compose
+
     echo "Docker Compose has been installed successfully."
 else
-    echo "Failed to install Docker Compose."
+    echo "Docker Compose is already installed."
 fi
 
 # Verify Docker Compose installation
