2e9e7c160da808dab7019de3b44c53e4781d8542
amrkedra <amrkedra.1993@gmail.com>
2024-09-19 12:19:32 +0300

    added the commands to install python3-disutils

--- compose/setup_docker_compose.sh
+++ compose/setup_docker_compose.sh
@@ -3,6 +3,10 @@
 # Update package list
 sudo apt update
 
+sudo apt upgrade
+
+sudo apt install -y python3 python3-pip python3-venv python3-setuptools -y
+
 # Check if Docker Compose is installed
 if ! command -v docker-compose &> /dev/null; then
     echo "Docker Compose not found, installing..."
